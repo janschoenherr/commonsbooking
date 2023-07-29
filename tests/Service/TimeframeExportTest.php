99060165534f62aec1018187497fcd6ece55ecfa
Hans Morbach <6433480+hansmorb@users.noreply.github.com>
2023-07-29 19:15:03 +0200

    first rudimentary tests

---
+++ tests/Service/TimeframeExportTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace CommonsBooking\Tests\Service;
+
+use CommonsBooking\Model\Booking;
+use CommonsBooking\Service\TimeframeExport;
+use CommonsBooking\Tests\Wordpress\CustomPostTypeTest;
+use CommonsBooking\Wordpress\CustomPostType\Timeframe;
+use DateTime;
+use stdClass;
+
+class TimeframeExportTest extends CustomPostTypeTest
+{
+	protected Booking $booking;
+
+	public function testGetCSV()
+	{
+		$yesterday = new DateTime(self::CURRENT_DATE);
+		$yesterday->modify('-1 day');
+		$tomorrow = new DateTime(self::CURRENT_DATE);
+		$tomorrow->modify('+1 day');
+		$export = new TimeframeExport(
+			Timeframe::BOOKING_ID,
+			$yesterday->format('Y-m-d'),
+			$tomorrow->format('Y-m-d')
+		);
+		$export->getExportData();
+		$csv = $export->getCSV();
+		$objects = $this->csvStringToStdObjects($csv);
+		$this->assertEquals(1, count($objects));
+		$exportedBooking = reset($objects);
+		$this->assertEquals($this->booking->ID, $exportedBooking->ID);
+
+	}
+
+	private function csvStringToStdObjects( $csvString ): array {
+		$rows   = explode( "\n", $csvString );
+		$header = str_getcsv( array_shift( $rows ), ';' );
+
+		$result = [];
+		foreach ( $rows as $row ) {
+			$data = str_getcsv( $row, ';' );
+			$obj  = new stdClass();
+
+			foreach ( $header as $index => $field ) {
+				$obj->$field = $data[ $index ] ?? null;
+			}
+
+			$result[] = $obj;
+		}
+
+		return $result;
+	}
+
+
+	protected function setUp(): void {
+		parent::setUp();
+		$this->booking = new Booking(
+			$this->createConfirmedBookingStartingToday()
+		);
+	}
+
+	protected function tearDown(): void {
+		parent::tearDown();
+	}
+}
